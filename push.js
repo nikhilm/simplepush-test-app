154e450d18905c9468c0c8e6af3fa46337f2bead
Nikhil Marathe <nsm.nikhil@gmail.com>
2013-07-16 16:55:00 -0700

    Event page based push

--- push.js
+++ push.js
@@ -1,18 +1,23 @@
 navigator.mozSetMessageHandler('push', function(e) {
+    //document.getElementById('theform').submit();
     localStorage.setItem("time", (new Date()).toString());
+    //$('#thelink').click();
 
-    var notification = navigator.mozNotification.createNotification('Push notification', 'From the SimplePush application');
+    var notification = navigator.mozNotification.createNotification('SimplePush: New IM', 'You got a new message from your best friend EVER! Open it please');
 
     notification.onclick = function test_notificationClick() {
-        console.log("FOOBARBAZ!");
+      //launch('/index.html', {message: 'eventPage says you got a message'});
     };
 
     notification.show();
-
-    var win = open('/index.html');
 });
 
 navigator.mozSetMessageHandler('push-register', function(e) {
-    var notification = navigator.mozNotification.createNotification('Lost all push registrations :(', 'From the SimplePush application');
+    var notification = navigator.mozNotification.createNotification('SimplePush', 'Lost all push registrations :(');
     notification.show();
 });
+
+onconnect = function(w) {
+  var notification = navigator.mozNotification.createNotification(w, 'Connected to a window');
+  //notification.show();
+}
