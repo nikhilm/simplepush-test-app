42387c4551536e998b7b49ff5ed87db672a3fc50
Nikhil Marathe <nsm.nikhil@gmail.com>
2014-02-03 18:19:49 -0800

    foo

--- service.js
+++ service.js
@@ -23,4 +23,28 @@ addEventListener("push-register", function(e) {
 
 addEventListener("install", function(e) {
   debug("Got installed really?!");
+  e.waitUntil(new Promise(function(res) {
+    setTimeout(res, 100);
+  }));
+});
+
+var base = "http://localhost:8000";
+var js = new URL("/data.js", base);
+var css = new URL("/assets/v1/404.css", base);
+
+addEventListener("fetch", function(e) {
+  // FIXME(nsm): url will be a URL at some point
+  if (e.request.url == js.href) {
+    debug("MATCH JS!");
+    e.respondWith(new SameOriginResponse({
+      statusCode: 200,
+      body: "alert('This is the fake deal.');"
+    }));
+  } else if (e.request.url == css.href) {
+    debug("MATCH CSS!");
+    e.respondWith(new SameOriginResponse({
+      statusCode: 200,
+      body: "body { background: red }"
+    }));
+  }
 });
